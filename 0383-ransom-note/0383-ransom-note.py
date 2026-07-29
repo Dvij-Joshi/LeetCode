ede37c2d3f7a88cc9b6af0010b7d4e8625f8ca76
Dvij Joshi <103959253+Dvij-Joshi@users.noreply.github.com>
2026-07-30 01:20:51 +0530

    Time: 82 ms (5.78%), Space: 19.9 MB (8.31%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        left=0
+        right=0
+        ransomNote=sorted(ransomNote)
+        magazine=sorted(magazine)
+        print(magazine)
+        print(ransomNote)
+        while right<len(magazine) and left<len(ransomNote):#1: 0<3 and 0<3, 2: 1<3 and 0<3
+            if ransomNote[left]==magazine[right]: # 1: if a==b. 2: a==bT
+                left+=1
+                right+=1
+            else:
+                right+=1 #1: right = 1
+        if left == len(ransomNote):
+                return True
+        else:
+            return False
