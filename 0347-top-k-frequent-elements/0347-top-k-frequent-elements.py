d9cb43777fa73b1f5cea02cc0451420706c17676
Dvij Joshi <103959253+Dvij-Joshi@users.noreply.github.com>
2026-08-29 17:30:18 +0530

    Time: 6 ms (56.68%), Space: 22.8 MB (80.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,10 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         fre={}
-        for num in nums:
-            if num in fre:
-                fre[num]+=1
-            else:
-                fre[num]=1
-        result=sorted(fre,key=fre.get,reverse=True)[:k]
+        # for num in nums:
+        #     if num in fre:
+        #         fre[num]+=1
+        #     else:
+        #         fre[num]=1
+        # result=sorted(fre,key=fre.get,reverse=True)[:k]
+        # return result
+        my_count = Counter(nums)
+        # print(my_count)
+        result=[item for item,count in my_count.most_common(k) ]
         return result
+        # return count.most_common(k)
